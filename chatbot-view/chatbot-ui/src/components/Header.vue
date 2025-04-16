bc9b055330558b6c12fe94e0001d46193626a3af
Xu <whitenight.xiaxu@gmail.com>
2025-04-16 15:53:06 +0800

    chat page

---
+++ chatbot-view/chatbot-ui/src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="py-4 px-6 bg-gray-800 shadow-md flex justify-between items-center"
+  >
+    <img :src="robotImage" alt="chat ai" class="w-8 h-8" />
+    <h1 class="text-lg font-semibold">Chat AI</h1>
+    <button @click="logout" class="text-gray-400 hover:text-white">
+      logout
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import robotImage from "../assets/robot.png"
+import { useUserStore } from "../stores/user"
+import { useRouter } from "vue-router"
+
+const userStore = useUserStore()
+const router = useRouter()
+const logout = () => {
+  userStore.logout()
+  router.push("/")
+}
+</script>
+
+<style lang="less" scoped></style>
--- chatbot-view/chatbot-ui/src/views/Chat.vue
+++ chatbot-view/chatbot-ui/src/views/Chat.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>chat</div>
+  <div class="flex flex-col h-screen bg-gray-900 text-white">
+    <Header />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from "../components/Header.vue"
+</script>
 
 <style lang="less" scoped></style>
